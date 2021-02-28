f2ea038048a8f36527276c1a15eb850ec05b90b2
Adam Joines <amountcastlej@gmail.com>
2021-02-28 12:26:33 -0500

    Python file added

---
+++ for_loop_basic1.py
@@ -0,0 +1,66 @@
+# 1. print all integers from 0 to 150
+#for i in range(0, 151):
+#    print(i)
+
+#2. print all multiples of 5 from 5 to 1000
+# create a for loop that gives all multiple of 5
+#for i in range(5, 1001, 5):
+#    print(i)
+
+#3. Counting, the Dojo Way 
+# Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo"
+# create a for loop that prints 1 to 100
+# create condition that if divides into 5 evenly print "Coding"
+# create condition that if divides into 10 evenly print "Coding Dojo"
+#for i in range(1, 101):
+#    if i % 5 == 0:
+#        print("Coding")
+#    elif i % 10 == 0:
+#        print("Coding Dojo")
+#    else: 
+#        print(i)
+
+#4. Whoa. That Sucker's Huge
+# Add odd integers from 0 to 500,000, and print the final sum
+# create a variable for odd numbers
+# create a for loop that prints 0 to 500,000
+# create condition that if odd add it to variable to hold odd numbers
+# Get the total of odd numbers and print the variable that holds the sum
+# oddTotal = 0
+# for i in range(0, 500000):
+#     if i % 2 == 1:
+#         oddTotal += i
+#     print(oddTotal)
+
+#5. Countdown by Fours
+# Print positive numbers starting at 2018, counting by fours
+# create a for loop 
+# start range at 2018, stop at 0, and subtract by 4 each time
+#for i in range(2018, 0, -4):
+#    print(i)
+
+#6. Flexible Counter 
+# Set three variables: lowNum, highNum, multi
+# Starting at lowNum and going through highNum, print only the integers that are multiple of mult
+# create variables lowNum, highNum, mult and assign integers to each
+# Use a for loop to check if mult is divisible by the numbers
+# If values are a multiple of mult aka 3 then print
+
+# lowNum = 2
+# highNum = 9
+# mult = 3
+
+# for num in range(lowNum, highNum + 1): 
+#     if num % mult == 0:
+#         print(num)
+
+
+
+
+
+
+
+
+
+
+
